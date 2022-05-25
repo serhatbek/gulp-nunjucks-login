f90ed3cb466de167dd9389ca6f4759dea2c6cc48
serhatbek <serhat.bekk@gmail.com>
2022-05-25 13:39:50 +0300

    change input error text color, add icon

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,4 @@
+// import * as $ from '../../node_modules/jquery/dist/jquery.min';
+// import parsley from '../../node_modules/parsley/dist/parsley';
 // *******************************
 $('#form-email').parsley();
