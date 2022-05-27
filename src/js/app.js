c0f82f5db5af31cec5fbf10922417a32aa9e55ec
serhatbek <serhat.bekk@gmail.com>
2022-05-27 09:37:46 +0300

    login password: validation

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,42 @@ $('#form-email')
   });
 // dsfdsgfg@gmail.com
 
+//has uppercase
+window.Parsley.addValidator('uppercase', {
+  requirementType: 'number',
+  validateString: function (value, requirement) {
+    var uppercases = value.match(/[A-Z]/g) || [];
+    return uppercases.length >= requirement;
+  },
+});
+
+//has lowercase
+window.Parsley.addValidator('lowercase', {
+  requirementType: 'number',
+  validateString: function (value, requirement) {
+    var lowecases = value.match(/[a-z]/g) || [];
+    return lowecases.length >= requirement;
+  },
+});
+
+//has number
+window.Parsley.addValidator('number', {
+  requirementType: 'number',
+  validateString: function (value, requirement) {
+    var numbers = value.match(/[0-9]/g) || [];
+    return numbers.length >= requirement;
+  },
+});
+
+//has special char
+window.Parsley.addValidator('special', {
+  requirementType: 'number',
+  validateString: function (value, requirement) {
+    var specials = value.match(/[^a-zA-Z0-9]/g) || [];
+    return specials.length >= requirement;
+  },
+});
+
 // **********************
 
 const input = document.querySelector('.input input');
